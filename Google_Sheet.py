5052895af326961e35e61bfe8e860e91afe822df
rsaks29 <57072351+rsaks29@users.noreply.github.com>
2024-01-30 11:25:11 +0100

    sheet update

--- Google_Sheet.py
+++ Google_Sheet.py
@@ -1,1 +1,17 @@
-# continue watching video I took notes on and implement that.
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES=["https://googleapis.com/auth/spreadsheets"]
+
+SPREATSHEET_ID="1EPYXbsuPgbFXJoghnMkAWnreqN253hXlf7uSXcebO-M"
+
+
+def main():
+    credentials=None
+    if os.path.exists("token.json"):
+        crendetials=Credentials.from_authorized_user_file("token.json", SCOPE)
